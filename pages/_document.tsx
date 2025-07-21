5605b66679a421a89807c9d3dd8f0faf81fed074
Kumaraguru <19eucs071@skcet.ac.in>
2025-07-22 00:07:23 +0100

    adding og tags for image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,26 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <script
-    src="https://cdn.databuddy.cc/databuddy.js"
-    data-client-id="RSYtwDlso7rAALfPWQjal"
-    data-enable-batching="true"
-    crossOrigin="anonymous"
-    async
-  ></script>
+        <script
+          src="https://cdn.databuddy.cc/databuddy.js"
+          data-client-id="RSYtwDlso7rAALfPWQjal"
+          data-enable-batching="true"
+          crossOrigin="anonymous"
+          async
+        ></script>
+
+        <meta property="og:url" content="https://codex-pinning.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/9a21ee8b-d03c-483a-94af-addece4627a6.png?token=hG5-v3SCR2_g_R-m1jo53hGlBS7UDCz7nmcnPhOwTyk&height=630&width=1200&expires=33289139111" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="codex-pinning.vercel.app" />
+        <meta property="twitter:url" content="https://codex-pinning.vercel.app" />
+        <meta name="twitter:title" content="" />
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/9a21ee8b-d03c-483a-94af-addece4627a6.png?token=hG5-v3SCR2_g_R-m1jo53hGlBS7UDCz7nmcnPhOwTyk&height=630&width=1200&expires=33289139111" />
       </Head>
       <body className="antialiased">
         <Main />
